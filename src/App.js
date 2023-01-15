4c215aac38f1b97d0758929f210a58f7812b2800
Hiroki9759 <hiroki338@gmail.com>
2023-01-15 14:32:57 +0900

    20230115

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Sample App</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            これはサンプルで作成したアプリです 
           </p>
           <a
             className="App-link"
